c0288b53a726f709385a37d6ee9da9504d643df3
Fiadh Almohanna <fiadh.mohanna@gmail.com>
2019-03-16 22:45:23 +0300

    Commit

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,20 +1,30 @@
 package com.udacity.sandwichclub;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import org.json.JSONException;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    public Sandwich sandwich = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +46,12 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        try {
+            sandwich = JsonUtils.parseSandwichJson(json);
+        } catch (JSONException e) {
+            closeOnError();
+            return;
+        }
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -57,6 +72,43 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
-
+        Uri uri;
+        ImageView imageView = findViewById(R.id.image_iv);
+        TextView lblAKA = findViewById(R.id.also_known_lbl),
+                 mAKA = findViewById(R.id.also_known_tv),
+                 lblPOO = findViewById(R.id.origin_lbl),
+                 mPOO = findViewById(R.id.origin_tv),
+                 lblDescription = findViewById(R.id.description_lbl),
+                 mDescription = findViewById(R.id.description_tv),
+                 lblIngredients = findViewById(R.id.ingredients_lbl),
+                 mIngredients = findViewById(R.id.ingredients_tv);
+        mAKA.setText("");
+        mIngredients.setText("");
+        uri = Uri.parse(sandwich.getImage());
+        imageView.setImageURI(uri);
+        setFieldList(lblAKA, mAKA, sandwich.getAlsoKnownAs());
+        setFieldText(lblPOO, mPOO, sandwich.getPlaceOfOrigin());
+        setFieldText(lblDescription, mDescription, sandwich.getDescription());
+        setFieldList(lblIngredients, mIngredients, sandwich.getIngredients());
+    }
+    public void setFieldText(TextView label, TextView field, String text){
+        label.setVisibility(View.VISIBLE);
+        field.setVisibility(View.VISIBLE);
+        if (text == null || text.equals("")){
+            label.setVisibility(View.GONE);
+            field.setVisibility(View.GONE);
+        } else
+            field.setText(text);
+    }
+    public void setFieldList(TextView label, TextView field, List<String> list){
+        field.setText("");
+        label.setVisibility(View.VISIBLE);
+        field.setVisibility(View.VISIBLE);
+        if (list == null || list.size() == 0){
+            label.setVisibility(View.GONE);
+            field.setVisibility(View.GONE);
+        } else
+            for (String text : list)
+                field.append(" - " + text + "\n");
     }
 }
