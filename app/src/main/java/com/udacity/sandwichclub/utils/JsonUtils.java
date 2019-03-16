6a5b77eb84a9a2cff6db0683ede7a8fc220fc32d
Fiadh Almohanna <fiadh.mohanna@gmail.com>
2019-03-16 22:45:18 +0300

    Commit

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,40 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        Sandwich sandwich = new Sandwich();
+        List<String> sandwichIngredientsList;
+        List<String> sandwichAKAList;
+        JSONObject sandwichObject = new JSONObject(json);
+        JSONObject sandwichNameObject = sandwichObject.getJSONObject("name");
+        sandwichIngredientsList = convertJsonToList(sandwichObject.getJSONArray("ingredients"));
+        sandwichAKAList = convertJsonToList(sandwichNameObject.getJSONArray("alsoKnownAs"));
+
+        sandwich.setMainName(sandwichNameObject.getString("mainName"));
+        sandwich.setPlaceOfOrigin(sandwichObject.getString("placeOfOrigin"));
+        sandwich.setDescription(sandwichObject.getString("description"));
+        sandwich.setIngredients(sandwichIngredientsList);
+        sandwich.setAlsoKnownAs(sandwichAKAList);
+        sandwich.setImage(sandwichObject.getString("image"));
+
+        return sandwich;
+    }
+    private static List<String> convertJsonToList(JSONArray jsonArray) throws JSONException {
+        List<String> list= new ArrayList<>();
+        if (jsonArray!=null){
+            for (int i=0;i<jsonArray.length();i++){
+                list.add(jsonArray.getString(i));
+            }
+        }
+        return list;
     }
 }
